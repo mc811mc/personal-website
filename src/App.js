098dac2e2ae4245914722ab975fdf4499d93687d
Minho Cho <mc811mc@gmail.com>
2021-07-09 17:13:38 +0900

    update in accordance to components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Minho Cho
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Enter
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, About, Travel, Programming } from "./components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/programming" exact component={() => <Programming />} />
+          <Route path="/travel" exact component={() => <Travel />} />
+          <Route path="/about" exact component={() => <About />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
